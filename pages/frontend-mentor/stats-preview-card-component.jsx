34cee907da8ded897f82be649a9c3722a8533a1e
kamekazz <taveras78@gmail.com>
2021-06-21 20:54:53 -0400

    v2

--- pages/frontend-mentor/stats-preview-card-component.jsx
+++ pages/frontend-mentor/stats-preview-card-component.jsx
@@ -4,33 +4,40 @@ import Head from 'next/head';
 
 export default function StatsPreviewComponent() {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <ReadSide />
-        <ImageSide />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className={styles.bodyContainer}>
+        <div className={styles.main}>
+          <ImageSide />
+          <TextContainer />
+        </div>
+        <div className={styles.deskPaddingBottom} />
       </div>
-    </div>
+    </>
   );
 }
 
-const ReadSide = () => {
+const TextContainer = () => {
   return (
-    <div className={styles.readSide}>
-      <div className={styles.textBox}>
-        <div>
-          <h3>
-            Get <span>insights</span> that help your business grow.
-          </h3>
-          <p>
-            discover the benefits of data analytics and made better decisions
-            regarding revenue,customer,experience,and overall efficiency
-          </p>
-        </div>
-        <div className={styles.Canters}>
-          <Canter amount={10} type={'companies'} value={'K+'} />
-          <Canter amount={314} type={'Templates'} value={''} />
-          <Canter amount={12} type={'Queries'} value={'M+'} />
-        </div>
+    <div className={styles.textContainer}>
+      <h1>
+        Get <span>insights</span> that help your business grow.
+      </h1>
+      <p className={styles.textBlock}>
+        Discover the benefits of data analytics and make better decisions
+        regarding revenue, customer experience, and overall efficiency.
+      </p>
+
+      <div className={styles.numbersContainer}>
+        <Canter amount={10} type={'companies'} value={'K+'} />
+        <Canter amount={314} type={'Templates'} value={''} />
+        <Canter amount={12} type={'Queries'} value={'M+'} />
       </div>
     </div>
   );
@@ -46,12 +53,9 @@ const Canter = ({ amount, type, value }) => {
     from: { opacity: 0, val: 0, color: '#ffffff' },
   });
   return (
-    <div className={styles.valueComponent}>
-      <div className={styles.valueRow}>
-        <animated.h3>{props.val.to((val) => Math.floor(val))}</animated.h3>
-        <animated.h3 style={props}>{value}</animated.h3>
-      </div>
-      <animated.p style={props}>{type}</animated.p>
+    <div className={styles.number}>
+      <p className={styles.textNumbers}>10k+</p>
+      <p className={styles.textCats}>companies</p>
     </div>
   );
 };
@@ -62,5 +66,19 @@ Canter.defaultProps = {
 };
 
 const ImageSide = () => {
-  return <div className={styles.imageSide}></div>;
+  return (
+    <div className={styles.hero}>
+      <img
+        className={styles.mobileImg}
+        src="/images/image-header-mobile.jpg"
+        alt="image-header-mobile"
+      />
+      <img
+        className={styles.desktopImg}
+        src="/images/image-header-desktop.jpg"
+        alt="image-header-desktop.jpg"
+      />
+      <div className={styles.heroOverlay} />
+    </div>
+  );
 };
